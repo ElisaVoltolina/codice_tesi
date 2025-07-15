c3747df31b7a73935ebddc6447bd5d862b654b47
Eli <eli.voltolina@gmail.com>
2025-07-15 11:57:15 +0200

    test

--- plot2.py
+++ plot2.py
@@ -2,4 +2,6 @@ from utils import load_solution, plot_darp_path_auto
 from main2 import x_solution,V, P, D
 # funziona si ma da migliorare ri ranna tutto il codice
 
-plot_darp_path_auto(x_solution, V, P, D)
+plot_darp_path_auto(x_solution, V, P, D)  
+
+#test
